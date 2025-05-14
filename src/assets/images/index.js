b018028cd8cf43b8a843872f8e39e9e5f292dd14
alvin kenyagah <alvin47k@gmail.com>
2025-05-14 15:01:30 +0300

    update image import

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,11 +1,11 @@
 import zanzibarHero from './zanzibar-hero.jpg';
-import stoneTown from './stone-town.jpg';
-import kilimanjaro from './kilimanjaro.jpg';
+// import stoneTown from './stone-town.jpg';
+// import kilimanjaro from './kilimanjaro.jpg';
 
 const images = {
   zanzibarHero,
-  stoneTown,
-  kilimanjaro
+//   stoneTown,
+//   kilimanjaro
 };
 
 export default images;
